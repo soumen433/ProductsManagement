4d14cdb0f6110f72bd634fb06bf0c5b4fbd1729f
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-05-25 13:53:11 +0530

    added : authorization ,updated :  validations

--- src/route/route.js
+++ src/route/route.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controller/userController")
-const authenticate = require("../middleware/middleware")
+const {authentication , authorization} = require("../middleware/middleware")
 const { createUserValid, loginValid, getValid, updateValid } = require("../middleware/validations")
 
 router.post("/register",createUserValid, userController.createUser)
 router.post("/login" ,loginValid, userController.loginUser)
 router.route("/user/:userId/profile")
-.get(authenticate.authentication, getValid, userController.getUser)
-.put(authenticate.authentication, updateValid, userController.updatedUser)
+.get(authentication, getValid, userController.getUser)
+.put(authentication, authorization, updateValid, userController.updatedUser)
 
 
 module.exports = router;
